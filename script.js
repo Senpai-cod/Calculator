87370d4d12cebd09ba941f30a99b326897be0387
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-03-09 08:55:17 +0100

    Add the operation function

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 }
+let a;
+let oper;
+let b;
+
+function operate(a,oper,b){
+    switch(oper){
+    case '+':
+     return add(a,b); 
+    case'-':
+     return substract(a,b); 
+    case "*":
+     return multiply(a,b); 
+    case "/":
+     return divide(a,b); 
+    default :
+     return "Please Enter a Valid operator";
+
+    }
+}
