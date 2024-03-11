a28fb733d7411726110c8872a41e0a0cca79e7bc
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-03-11 10:51:14 +0100

    Add a display when clicking buttons

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ function operate(a,oper,b){
 
     }
 }
+const screen = document.querySelector("input");
+const buttons = document.querySelectorAll("button");
+screen.value = "calculate";
+screen.style.color = "gray";
+
+let text = "";
+let a1;
+const b1 = "";
+buttons.forEach((button) =>{
+    
+   
+    button.addEventListener("click", ()=>{
+        
+        screen.style.color = "black";
+        a1 = button.textContent;
+        text = text + button.textContent ;
+        screen.value = text ;
+    })
+    
+})
