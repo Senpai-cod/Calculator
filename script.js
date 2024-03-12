34356ebdf9e9deb0cb4024dbd9f8708de0fd24ce
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-03-12 12:00:27 +0100

    Madethe calculator work by doing calculations when the user click the buttons

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function operate(a,oper,b){
      return add(a,b); 
     case'-':
      return substract(a,b); 
-    case "*":
+    case "x":
      return multiply(a,b); 
     case "/":
      return divide(a,b); 
@@ -34,18 +34,77 @@ const buttons = document.querySelectorAll("button");
 screen.value = "calculate";
 screen.style.color = "gray";
 
+let operator= "undefined";
 let text = "";
-let a1;
-const b1 = "";
+let a1 = "";
+let b1 = "";
+let result = "undefined";
 buttons.forEach((button) =>{
     
-   
     button.addEventListener("click", ()=>{
-        
+        if(button.textContent == "="){
+           
+          a1 = Number(a1);
+          b1 = Number(b1);
+          result =  operate(a1,operator,b1); 
+          if(toString(result).length > 8){
+            result = Math.round(Number(result));
+          }
+          operator = "undefined";
+          b1 = "";     
+        }     
         screen.style.color = "black";
-        a1 = button.textContent;
-        text = text + button.textContent ;
-        screen.value = text ;
+
+if(operator == "undefined"){
+    if(result != "undefined"){
+        a1 = result;
+        
+    }
+    else if((button.textContent >= "0" && button.textContent <= "9" )|| button.textContent == "."){
+        
+         a1 += button.textContent
+    }
+}
+    if(button.textContent == "+" ||
+    button.textContent == "-" ||
+    button.textContent == "x" ||
+    button.textContent == "/" ){
+        operator = button.textContent;
+    }
+    if(operator != "undefined"){
+            if((button.textContent >= "0" && button.textContent <= "9" )|| button.textContent == "."){
+        b1 += button.textContent
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+        if(button.textContent == "="){
+            text = result;
+        }
+        else if(button.textContent == "Clear"){
+         text= "";
+         operator= "undefined";
+         a1 = "";
+         b1 = "";
+         result = "undefined";
+         screen.value = "calculate";
+         screen.style.color = "gray";
+        }
+        else text = text + button.textContent ;
+         
+        if(button.textContent != "Clear")screen.value = text ;
+
+
+        result = "undefined";
     })
-    
+
 })
